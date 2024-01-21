a5959745409cee134a93504c857e00b14284733b
Pau <pauguvi@gmail.com>
2024-01-21 16:20:09 +0100

    remove deprecated collaborate package

--- Assets/Scripts/CameraZones.cs
+++ Assets/Scripts/CameraZones.cs
@@ -1,21 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraZones : MonoBehaviour
 {
     public GameObject player;
-    public GameObject camera;
+    //public GameObject camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
